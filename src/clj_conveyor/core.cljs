da1e5b6ffcecc934abfac8b37b75e5a96b24468e
eugene_potapenko <>
2018-01-10 21:35:46 +0100

    in progress

--- src/clj_conveyor/core.cljs
+++ src/clj_conveyor/core.cljs
@@ -19,46 +19,59 @@
 
 (defprotocol ^:private IConveyor
   (init [this])
-  (add [this cb t args])
+  (add [this cb args] [this cb t args])
   (pause [this t])
   (stop [this])
   (play [this])
   (clean [this])
   (clean-and-stop [this]))
 
-(defn- ->state [id]
-  (if-let [res (get @state id)]
-    res
-    (let [conv-state {:commands (chan) :played true}]
-      (swap! state assoc id conv-state)
-      conv-state)))
-
 (defn- wait-timeout [t]
   (go
     (cond
-      (number? t)          (<! (timeout t))
+      (number? t)            (<! (timeout t))
       (and
        (string? t)
        (re-find #"^\d+$" t)) (dotimes [x (js/parseInt t)]
-       (<! (await-cb request-animation-frame)))
-      :else (js/console.error "Invalid conv timeout format:" t))
+                               (<! (await-cb request-animation-frame)))
+      (nil? t)               :nothing
+      :else                  (js/console.error "Invalid conv timeout format:" t))
     "waiting end"))
 
-(defn- stack-call [t args]
-  (go
-    (<! (wait-timeout t))
+(defn- ->state [id]
+  (get @state id))
 
-
-    ))
+(defn- get-last-stack [id]
+  (-> (->state id) :stack last))
 
 (deftype Conveyor [id]
   IConveyor
-  (init [this] this)
-  (add [this cb t args])
-  (pause [this t] this)
-  (stop [this] this)
-  (play [this] this)
-  (clean [this] this)
+  (init [this]
+    (swap! state assoc id {:stack [(chan)] :played true :counter 0 :pause-chan (chan)})
+    (go-loop []
+      (let [[cb t args] (<! (get-last-stack id))]
+        (<! (wait-timeout t))
+        (let [c (-> id ->state :counter)]
+          (swap! state update-in [id :stack] [] [(chan)])
+          (apply cb args)
+          (when (not= c (-> id ->state :counter))
+
+            )))
+      (recur))
+    this)
+  (add [this cb args] (add this cb nil args))
+  (add [this cb t args]
+    (swap! state update-in [id :counter] inc)
+    (put! (get-last-stack) [cb t args])
+    this)
+  (pause [this t] this (add this t #() []))
+  (stop [this]
+    (swap! state assoc-in [id :played] false) this)
+  (play [this]
+    (swap! state assoc-in [id :played] true)
+    (put! (-> id ->state :pause-chan) [cb t args])
+    this)
+  (clean [this] (init id) this)
   (clean-and-stop [this]
     (-> this clean stop)))
 
